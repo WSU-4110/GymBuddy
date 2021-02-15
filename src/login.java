c82289eec84a6d6d6c212021b9a2d8c468c2a2b0
Ragib Miah <ragib@wayne.edu>
2021-02-14 23:19:56 -0500

    Sample validation and login

---
+++ src/login.java
@@ -0,0 +1,19 @@
+public class login {
+
+    private String username;
+    private String password;
+
+    protected void setUsername(String username){
+        this.username = username;
+    }
+    public String getUsername(){
+        return username;
+    }
+
+    protected void setPassword(String password){
+        this.password= password;
+    }
+    public String getPassword(){
+        return password;
+    }
+}
