965ee87983839309670d1a19b80d45b32d72583c
KyleCarr <kyle.carr98@gmail.com>
2021-04-06 13:21:48 -0400

    updated project to work with android studio and firebase

--- app/src/main/java/com/example/gymbuddy/MainActivity.kt
+++ app/src/main/java/com/example/gymbuddy/MainActivity.kt
@@ -19,7 +19,6 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
     }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -35,4 +34,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
