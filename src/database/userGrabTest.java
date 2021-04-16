b04ea6d78ca8a8b704efd4cb844b7b4762b7695c
KyleCarr <kyle.carr98@gmail.com>
2021-04-16 09:24:50 -0400

    added unit test functions for assignment 6

--- src/database/userGrabTest.java
+++ src/database/userGrabTest.java
@@ -1,5 +1,14 @@
+package database;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class userGrabTest {
 
+    @Test
+    public void grab() {
+        assertEquals(database.userGrab.grab("kyle","password123"),database.userGrab.grab("kyle","password123"));
+        assertNotEquals(database.userGrab.grab("admin","password123"),database.userGrab.grab("kyle","password123"));
+    }
 }
