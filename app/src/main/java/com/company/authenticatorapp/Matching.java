f946bce28548abfc0fbd97a8dced8415d79e1467
KyleCarr <kyle.carr98@gmail.com>
2021-04-19 13:44:48 -0400

    fixed issues with matching not pulling a random user properly

--- app/src/main/java/com/company/authenticatorapp/Matching.java
+++ app/src/main/java/com/company/authenticatorapp/Matching.java
@@ -22,36 +22,24 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
-public class Matching extends AppCompatActivity{
+import java.util.ArrayList;
+import java.util.Random;
 
-    public void userName(){
-        setContentView(R.layout.matching);
-        TextView textView = (TextView) findViewById(R.id.textView4);
+public class Matching extends AppCompatActivity{
+public DatabaseReference randomRef;
+    public DatabaseReference userName(){
+
         FirebaseDatabase reference = FirebaseDatabase.getInstance();
         DatabaseReference myRef = reference.getReference("Users/" + FirebaseAuth.getInstance().getCurrentUser().getUid());
         //String ID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        myRef.child("Name").get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
-            @Override
-            public void onSuccess(DataSnapshot dataSnapshot) {
-                String name =(String.valueOf(dataSnapshot.getValue())); //only one user name displays at a time fix this tomorrow
-                textView.setText(name);
-            }
-        });
+       return myRef;
     }
+
+    public DatabaseReference matchingName(){
 //O9vCI2bybBbFMxdmZv7paNh9vsF2
-    public void matchingName(){
-        setContentView(R.layout.matching);
-        TextView textView = (TextView) findViewById(R.id.textView5);
         FirebaseDatabase reference = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = reference.getReference("Users/O9vCI2bybBbFMxdmZv7paNh9vsF2");
-        //String ID = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        myRef.child("Name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                String name =(String.valueOf(task.getResult().getValue()));
-                textView.setText(name);
-            }
-        });
+        DatabaseReference myRef = reference.getReference();
+        return myRef;
     }
 
 
@@ -59,8 +47,59 @@ public class Matching extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        userName();
-        matchingName();
+        setContentView(R.layout.matching);
+        TextView textView = (TextView) findViewById(R.id.textView4);
+        TextView textView2 = (TextView) findViewById(R.id.textView5);
+
+        reference(userName() , textView);
+        random(matchingName(), textView2);
+
 
     }
+
+public void reference(DatabaseReference myRef, TextView textView){
+    myRef.child("Name").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<DataSnapshot> task) {
+            String name =(String.valueOf(task.getResult().getValue()));
+            textView.setText(name);
+        }
+
+
+    });
+}
+public void random(DatabaseReference myRef, TextView textView){
+    FirebaseDatabase reference = FirebaseDatabase.getInstance();
+    myRef.child("Users/").addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            Random random;
+            ArrayList list = new ArrayList<String>();
+            for(DataSnapshot arr: snapshot.getChildren()){
+                String s1 = arr.toString().substring(arr.toString().indexOf("=")+1);
+                s1 = s1.split(",")[0];
+                s1 = s1.replaceAll(" ", "");
+                list.add(s1);
+            }
+            random = new Random();
+            // String key = new String(String.valueOf(list.get(0)));
+            String[] array = new String[list.size()];
+            int index = 0;
+            for(Object Value: list){
+                array[index] = (String) Value;
+                System.out.println(array[index]);
+                index++;
+            }
+            System.out.println("line 90");
+            String key = array[(int)Math.floor(Math.random()*(array.length))];
+            randomRef = reference.getReference("Users/" + key);
+           reference(randomRef, textView);
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    });
+}
 }
