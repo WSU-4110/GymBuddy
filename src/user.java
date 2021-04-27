854cb34646d52e8e545dbe054aab908a20d16ec0
KyleCarr <kyle.carr98@gmail.com>
2021-04-27 19:48:28 -0400

    fixed merge issue

--- src/user.java
+++
@@ -1,8 +0,0 @@
-public interface user {
-
-    void workoutRoutine();
-    void userInfo(String userFirstName, String userLastName, String userPhoneNumber, String userEmail,
-                  String userGender, int userBirthdayYear, int userBirthdayMonth, int userBirthdayDay);
-}
-
-
